93e0bd14bed7399aa47d3c59fb88652219fe8b63
David Vidal <David Vidal>
2016-07-12 20:06:44 +0200

    testing collisions

---
+++ Assets/Scripts/Health/DamageOnCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageOnCollision : MonoBehaviour
+{
+    public int _damageOnCollision = 1;
+
+    private Health _healthScript;
+
+    void Awake()
+    {
+        _healthScript = GetComponent<Health>();
+    }
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        print("Colliding");
+        if (other.CompareTag("Enemy"))
+        {
+            _healthScript.TakeDamage(_damageOnCollision);
+        }
+    }
+}
---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+    public int _maxHP = 10;
+    public int _currentHP;
+    public bool _bDead = false;
+    public bool _bHit = false;
+    public float _hitCD = 1f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InitHealth();
+	}
+	
+    private void InitHealth()
+    {
+        _currentHP = _maxHP;
+        _bDead = false;
+        _bHit = false;
+    }
+
+    public void TakeDamage(int pAmount)
+    {
+        if (!_bDead && !_bHit)
+        {
+            _currentHP -= pAmount;
+            if (_currentHP < 0)
+            {
+                _currentHP = 0;
+                _bDead = true;
+            }
+            _bHit = true;
+            StartCoroutine(RestoreHit());
+        }
+    }
+
+    public void Heal(int pAmount)
+    {
+        if (!_bDead)
+        {
+            _currentHP += pAmount;
+            if (_currentHP > _maxHP)
+            {
+                _currentHP = _maxHP;
+            }
+        }
+    }
+
+    private IEnumerator RestoreHit()
+    {
+        yield return new WaitForSeconds(_hitCD);
+        _bHit = false;
+    }
+}
